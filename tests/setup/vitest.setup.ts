aa691e730898336ebf88e837e9cfbe1d6c1dfae5
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-09-17 10:20:12 -0300

    config: configure package aliases

--- tests/setup/vitest.setup.ts
+++ tests/setup/vitest.setup.ts
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
 import { afterAll, afterEach, beforeAll } from 'vitest'
 
-import { server } from '../mocks/server'
+import { server } from '@test/mocks/server'
 
 beforeAll(() => {
   server.listen({ onUnhandledRequest: 'error' })
