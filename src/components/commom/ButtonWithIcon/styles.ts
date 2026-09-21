197dbb9303132c08f8dc15cf0c7520fb63451a61
Luigi Biondo <luigi.biondo@jbs.com.br>
2026-09-21 17:45:43 -0300

    adding ButtonWithIcon component

---
+++ src/components/commom/ButtonWithIcon/styles.ts
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components";
+
+import { ButtonComponent } from "@/components/commom/ButtonComponent";
+
+interface StyledWrapperProps {
+  height?: string;
+}
+
+export const Wrapper = styled(ButtonComponent)<StyledWrapperProps>(
+  ({ theme, height }) => css`
+    && {
+      height: ${height && height};
+      svg {
+        font-size: ${theme.font.size.pageTab};
+      }
+    }
+  `
+);
---
+++ src/components/commom/ButtonWithIcon/test.tsx
@@ -0,0 +1,13 @@
+import { renderWithTheme } from "@/utils/__tests__/helpers";
+import { screen } from "@testing-library/react";
+import { Add } from "@mui/icons-material";
+import { ButtonWithIcon } from ".";
+import { describe, it, expect } from 'vitest'
+
+describe("<ButtonWithIcon />", () => {
+  it("should be rendered", () => {
+    renderWithTheme(<ButtonWithIcon icon={<Add />}>Adicionar</ButtonWithIcon>);
+
+    expect(screen.getByText("Adicionar"));
+  });
+});
