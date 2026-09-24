bd940f52a356b88f7f886676d5f9e0fd57b658fa
Luigi Biondo <luigi.biondo@jbs.com.br>
2026-09-23 21:54:12 -0300

    adding TextInput component

---
+++ src/components/commom/TextInputComponent/styles.ts
@@ -0,0 +1,13 @@
+import { TextField } from "@mui/material";
+import styled, { css } from "styled-components";
+
+export const TextInput = styled(TextField)(
+  ({ theme }) => css`
+    && {
+      input,
+      label {
+        font-size: ${theme.font.size.body1};
+      }
+    }
+  `,
+);
