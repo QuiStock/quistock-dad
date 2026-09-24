fba454260fcf4292b835fb0d86b4d61c771bcf75
Luigi Biondo <luigi.biondo@jbs.com.br>
2026-09-24 10:08:37 -0300

    adding reduced dashboards

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,13 @@
 import { Page } from "@/components/commom/Page"
 import { StoresPartial } from "@/partials/StoresPartial"
+import ReducedDashboard from "@/components/home/ReducedDashboard"
 
 const Home = () => {
     return (
-        <Page title="Home" children={<StoresPartial />} />
+        <Page title="Home" children={<>
+            <ReducedDashboard />
+            <StoresPartial />
+        </>} />
     )
 }
 
